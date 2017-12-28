0c632251addc6cb2d294f7e9bc63724fb5e46330
Alexander Menze <a.menze@polipol.de>
2017-12-28 20:57:38 +0100

    Created custom entity tracker objects which catch move packets

---
+++ src/de/dca/entitytags/entitytracker/CustomEntityTracker.kt
@@ -0,0 +1,70 @@
+package de.dca.entitytags.entitytracker
+
+import net.minecraft.server.v1_12_R1.EntityTracker
+import net.minecraft.server.v1_12_R1.EntityTrackerEntry
+import net.minecraft.server.v1_12_R1.IntHashMap
+import net.minecraft.server.v1_12_R1.WorldServer
+import org.bukkit.World
+import org.bukkit.craftbukkit.v1_12_R1.CraftWorld
+import java.lang.reflect.Field
+import java.util.*
+
+class CustomEntityTracker
+    : EntityTracker{
+
+    private lateinit var superTrackerSet: Set<EntityTrackerEntry>
+    private lateinit var superTrackerMap: IntHashMap<EntityTrackerEntry>
+
+    private val tmpTrackerSet: HashSet<EntityTrackerEntry> = HashSet()
+
+    companion object {
+
+        private val trackerSetField: Field
+
+        init {
+            trackerSetField = findTrackerSetField()
+            trackerSetField.isAccessible = true
+        }
+
+        private fun findTrackerSetField() : Field {
+            try {
+                return EntityTracker::class.java.getDeclaredField("c")
+            }catch (ex: Exception){
+                throw RuntimeException(ex)
+            }
+        }
+
+    }
+
+    constructor(worldServer: WorldServer)
+        : super(worldServer)
+    {
+        checkTrackerEntries()
+    }
+
+    constructor(world: World)
+        : this((world as CraftWorld).handle)
+
+    private fun loadTrackersFromSuper(){
+        try {
+            superTrackerSet = trackerSetField.get(this) as Set<EntityTrackerEntry>
+            superTrackerMap = this.trackedEntities
+        }catch (ex: Exception){
+            throw RuntimeException(ex)
+        }
+    }
+
+    private fun checkTrackerEntries(){
+        loadTrackersFromSuper()
+        tmpTrackerSet.clear()
+
+        val iter: Iterator<EntityTrackerEntry> = superTrackerSet.iterator()
+        while (iter.hasNext()){
+            val it = iter.next()
+            if (it is CustomEntityTrackerEntry)
+                continue
+
+
+        }
+    }
+}
--- src/de/dca/entitytags/entitytracker/CustomEntityTrackerEntry.kt
+++ src/de/dca/entitytags/entitytracker/CustomEntityTrackerEntry.kt
@@ -1,4 +1,42 @@
 package de.dca.entitytags.entitytracker
 
-class CustomEntityTrackerEntry {
+import de.dca.entitytags.util.ReflectionUtil
+import net.minecraft.server.v1_12_R1.*
+import org.bukkit.entity.LivingEntity
+
+class CustomEntityTrackerEntry
+    : EntityTrackerEntry {
+
+    constructor(entity: Entity, i: Int, j: Int, k: Int, flag: Boolean)
+        : super(entity, i, j, k, flag)
+
+    private constructor(entity: Entity)
+        : super(entity, 0, 0, 0, false)
+
+    override fun broadcast(packet: Packet<*>?) {
+        super.broadcast(packet)
+        handlePacket(packet)
+    }
+
+    private fun handlePacket(packet: Packet<*>?){
+        if (packet is PacketPlayOutEntity.PacketPlayOutRelEntityMoveLook
+                || packet is PacketPlayOutEntity.PacketPlayOutRelEntityMove
+                || packet is PacketPlayOutEntityTeleport){
+
+            val entity = this.b() as? EntityLiving ?: return
+            val bukkitEntity = entity.bukkitEntity as? LivingEntity ?: return
+
+            //Todo: Check entitytags
+        }
+    }
+
+    fun copyFrom(oldEntry: EntityTrackerEntry) : CustomEntityTrackerEntry {
+        val newEntry = CustomEntityTrackerEntry(oldEntry.b())
+        try {
+            ReflectionUtil.copyObjectInto(oldEntry, newEntry, EntityTrackerEntry::class.java)
+        }catch (ex: Exception){
+            throw RuntimeException("Could not create custom entity tracker entry!", ex)
+        }
+        return newEntry
+    }
 }
