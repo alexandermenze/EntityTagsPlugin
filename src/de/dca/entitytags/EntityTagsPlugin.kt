e620b796030b3f9cdaa08b7b40914df6024a597d
Alexander Menze <a.menze@polipol.de>
2017-12-30 15:02:56 +0100

    Added instantiation of a CustomEntityTrackerInjector on plugin load

--- src/de/dca/entitytags/EntityTagsPlugin.kt
+++ src/de/dca/entitytags/EntityTagsPlugin.kt
@@ -1,11 +1,19 @@
 package de.dca.entitytags
 
+import de.dca.entitytags.entitytracker.CustomEntityTrackerInjector
 import org.bukkit.plugin.java.JavaPlugin
 
 class EntityTagsPlugin : JavaPlugin() {
 
-    override fun onEnable() {
+    private lateinit var customEntityTrackerInjector: CustomEntityTrackerInjector
 
+    override fun onEnable() {
+        customEntityTrackerInjector = CustomEntityTrackerInjector(this)
+        customEntityTrackerInjector.injectInLoadedWorlds()
+    }
+
+    override fun onDisable() {
+        customEntityTrackerInjector.dispose()
     }
 
 }
