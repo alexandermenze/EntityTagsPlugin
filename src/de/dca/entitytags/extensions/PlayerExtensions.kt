ca2d5540b403073014d73a8739106ab92fdd394c
Alexander Menze <a.menze@polipol.de>
2018-01-21 14:20:01 +0100

    Removed external PlayerConnectionWrapper and replaced

--- src/de/dca/entitytags/extensions/PlayerExtensions.kt
+++ src/de/dca/entitytags/extensions/PlayerExtensions.kt
@@ -1,7 +1,8 @@
 package de.dca.entitytags.extensions
 
-import de.dca.entitytags.data.PlayerNetworkWrapper
+import net.minecraft.server.v1_12_R1.PlayerConnection
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer
 import org.bukkit.entity.Player
 
-val Player.NetworkWrapper : PlayerNetworkWrapper
-  get() = PlayerNetworkWrapper(this)
+val Player.NetworkWrapper : PlayerConnection
+  get() = (this as CraftPlayer).handle.playerConnection
