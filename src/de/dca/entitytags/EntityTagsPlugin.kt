ee4b9f0c017072ac297de5697eb25128c41bc2d6
Alexander Menze <a.menze@polipol.de>
2018-02-04 12:55:56 +0100

    Implemented EntityTagsEventManager

--- src/de/dca/entitytags/EntityTagsPlugin.kt
+++ src/de/dca/entitytags/EntityTagsPlugin.kt
@@ -1,19 +1,33 @@
 package de.dca.entitytags
 
+import de.dca.entitytags.api.EntityTags
 import de.dca.entitytags.entitytracker.CustomEntityTrackerInjector
+import de.dca.entitytags.listeners.EntityTagsEventManager
+import de.dca.events.CustomEventManager
 import org.bukkit.plugin.java.JavaPlugin
 
 class EntityTagsPlugin : JavaPlugin() {
 
     private lateinit var customEntityTrackerInjector: CustomEntityTrackerInjector
+    private lateinit var entityTagsEventManager: EntityTagsEventManager
+    private lateinit var customEventManager: CustomEventManager
 
     override fun onEnable() {
+        customEventManager = CustomEventManager()
+        customEventManager.onEnable(this)
+
         customEntityTrackerInjector = CustomEntityTrackerInjector(this)
         customEntityTrackerInjector.injectInLoadedWorlds()
+
+        entityTagsEventManager = EntityTagsEventManager(this)
     }
 
     override fun onDisable() {
         customEntityTrackerInjector.dispose()
-    }
+        entityTagsEventManager.dispose()
 
+        customEventManager.onDisable()
+
+        EntityTags.globalDispose()
+    }
 }
--- src/de/dca/entitytags/listeners/EntityTagsEventManager.kt
+++ src/de/dca/entitytags/listeners/EntityTagsEventManager.kt
@@ -1,2 +1,64 @@
 package de.dca.entitytags.listeners
 
+import de.dca.entitytags.api.EntityTags
+import de.dca.events.PlayerBlockMoveEvent
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.EventPriority
+import org.bukkit.event.HandlerList
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDeathEvent
+import org.bukkit.event.player.PlayerChangedWorldEvent
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.event.player.PlayerKickEvent
+import org.bukkit.event.player.PlayerQuitEvent
+import org.bukkit.plugin.Plugin
+
+class EntityTagsEventManager : Listener {
+
+    constructor(plugin: Plugin){
+        plugin.server.pluginManager.registerEvents(this, plugin)
+    }
+
+    fun dispose(){
+        HandlerList.unregisterAll(this)
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    private fun onEntityDie(event: EntityDeathEvent){
+        if(!event.entity.isDead)
+            return
+
+        EntityTags.remove(event.entity)
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    private fun onPlayerBlockMove(event: PlayerBlockMoveEvent){
+        EntityTags.internalPlayerUpdate(event.player)
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    private fun onPlayerJoin(event: PlayerJoinEvent) {
+        EntityTags.internalPlayerUpdate(event.player)
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    private fun onPlayerLeft(event: PlayerQuitEvent){
+        handlePlayerLeft(event.player)
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    private fun onPlayerKick(event: PlayerKickEvent){
+        handlePlayerLeft(event.player)
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    private fun onPlayerChangedWorlds(event: PlayerChangedWorldEvent){
+        EntityTags.internalPlayerUpdate(event.player)
+    }
+
+    private fun handlePlayerLeft(player: Player){
+        EntityTags.internalPlayerLeft(player)
+        EntityTags.remove(player)
+    }
+}
